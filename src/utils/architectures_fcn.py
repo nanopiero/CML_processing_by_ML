4a39c705841b53694f824f951975e0eaed87aafe
nanopiero <136181914+nanopiero@users.noreply.github.com>
2024-10-02 15:04:30 +0200

    init the final percpetron

--- src/utils/architectures_fcn.py
+++ src/utils/architectures_fcn.py
@@ -365,7 +365,11 @@ class UNet_causal_5mn_atrous_multiplicative_rescale(nn.Module):
         self.p = nn.Parameter(torch.ones(16))
         self.num_cmls = num_cmls
         self.linears1 = nn.ModuleList([nn.Linear(self.input_size_fc_layer, self.hidden_size_fc_layer) for i in range(self.num_cmls + 1)])
+        self.linears1.weight.data.fill_(0.)
+        self.linears1.bias.data.fill_(0.)
         self.linears2 = nn.ModuleList([nn.Linear(self.hidden_size_fc_layer, 1) for i in range(self.num_cmls + 1)])
+        self.linears2.weight.data.fill_(0.)
+        self.linears2.bias.data.fill_(0.)
         self.fixed_cumul = fixed_cumul
         self.pad_size = 20 - 1
         self.relu = nn.ReLU()
